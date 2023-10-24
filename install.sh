a640fd114f77815ec212af9a8f18f22eff14d284
mishaszu <mishaszu@gmail.com>
2023-10-24 09:50:21 +0200

    update install & update scripts

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ mkdir -p ~/.vim/backup
 mkdir -p ~/.undodir
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.config/nvim
+touch ~/.config/nvim/init.vim
+FILE="~/.config/nvim/init.vim"
+if [-f "$FILE"]; then
+    echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath = &runtimepath\nsource ~/.vimrc" > ~/.config/nvim/init.vim
+fi
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
-str="-p"
+str="--save"
+str2="--overwrite"
 
 NOW=$(date +"%Y-%m-%d-%m-%s")
 LOGFILE="vim-backup-$NOW"
@@ -11,10 +12,13 @@ if [ "$1" = "$str" ];
     echo "Copied global configuration as repo configuration"
   elif [ "$1" = "-h" ];
   then
-    echo "- '-p' for coping current configuration to repo\n- no '-p' for coping repo configuration as current"
-  else
+    echo "- '--save' for coping current configuration to repo\n- no '-p' for coping repo configuration as current"
+  elif [ "$1" = "$str2" ];
+  then
     cp ~/.vimrc ./backup/"$LOGFILE"
     cp ./vimrc ~/.vimrc
     echo "Copied repo configuration as global configuration"
+  else
+    echo "- '--save' for coping current configuration to repo\n- no '-p' for coping repo configuration as current"
 fi
 
